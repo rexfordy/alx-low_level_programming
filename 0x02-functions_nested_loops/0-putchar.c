a0b7048ccaeeca7285a7e5c0590f547c9ee0b661
Linus Atte <linuseatte@gmail.com>
2023-06-20 15:24:03 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* main - Entry point
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+char shty = "_putchar";
+while (shty)
+{
+_putchar(shty);
+shty;
+}
+_putchar('\n');
+return (0);
+}
