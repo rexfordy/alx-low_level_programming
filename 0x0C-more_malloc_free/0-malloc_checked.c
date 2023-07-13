81ba240f7561c650f593e8ecbbf663f11735e55d
Linus Atte <linuseatte@gmail.com>
2023-07-13 16:29:58 -0700

    malloc checked 2

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,30 +1,20 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 
 /**
-* main - check the code
-* Return: Always 0.
-*/
-int main(void)
+ * malloc_checked - Creates an array based on passed in value
+ * @b: Value passed in for array creation
+ * Return: nothing
+ */
+void *malloc_checked(unsigned int b)
 {
-char *c;
-int *i;
-float *f;
-double *d;
+	void *ptr = NULL;
 
-c = malloc_checked(sizeof(char) * 1024);
-printf("%p\n", (void *)c);
-i = malloc_checked(sizeof(int) * 402);
-printf("%p\n", (void *)i);
-f = malloc_checked(sizeof(float) * 100000000);
-printf("%p\n", (void *)f);
-d = malloc_checked(INT_MAX);
-printf("%p\n", (void *)d);
-free(c);
-free(i);
-free(f);
-free(d);
-return (0);
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
 }
