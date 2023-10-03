b6bd8eb429b2a9749cba25ad120f1ff409f68bc0
Linus Atte <linuseatte@gmail.com>
2023-10-02 18:15:16 -0700

    Append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,32 +1,32 @@
 #include "main.h"
 /**
- * append_text_to_file - append a textfile
- * @filename: the textfile to write.
- * @text_content: the content of the textfile to write
- * Description: append content to a textfile
- * section header: the header is main.h
- * Return: 1 in success -1 in failure
- */
+* append_text_to_file - append a textfile
+* @filename: the textfile to write.
+* @text_content: the content to be write
+* Description: append content of  a textfile
+* section header: the header is main.h
+* Return: 1 in success -1 in failure
+*/
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fot, _write, t; /* t : the number of bytes of the content */
+int fd, t, _write ; /* t : the number of bytes of the content */
 
-	if (filename == NULL)
-		return (-1);
+if (filename == NULL)
+return (-1);
 
-	fot = open(filename, O_WRONLY | O_APPEND);
-	if (fot == -1)
-		return (-1);
+fd = open(filename, O_WRONLY | O_APPEND);
+if (fd == -1)
+return (-1);
 
-	if (text_content)
-	{
-		while (text_content[t])
-			t++;
+if (text_content)
+{
+while (text_content[t])
+t++;
 
-		_write = write(fot, text_content, t);
-		if (_write == -1)
-			return (-1);
-	}
-	close(fot);
-	return (1);
+_write = write(fd, text_content, t);
+if (_write == -1)
+return (-1);
+}
+close(fd);
+return (1);
 }
