678714f37699cbabec8349feab2479361581498d
Linus Atte <linuseatte@gmail.com>
2023-10-02 17:36:52 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,32 +1,36 @@
 #include "main.h"
 /**
- * create_file - write a textfile
+ * append_text_to_file - append a textfile
  * @filename: the textfile to write.
  * @text_content: the content of the textfile to write
- * Description: write  a filefile
- * section header: the header of is main.h
+ * Description: append content to a textfile
+ * section header: the header is main.h
  * Return: 1 in success -1 in failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fot, _write, t; /* t: is the len of the content */
+int fot, _write, t = 0; /* Initialize t to 0 */
 
-	if (filename == NULL)
-		return (-1);
+if (filename == NULL)
+return (-1);
 
-	fot = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (fot == -1)
-		return (-1);
-
-	if (text_content != NULL)
-	{
-		while (text_content[t])
-			t++;
-
-		_write = write(fot, text_content, t);
-		if (_write == -1)
-			return (-1);
-	}
-	close(fot);
-	return (1);
+fot = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+if (fot == -1)
+return (-1);
+if (text_content != NULL)
+{
+while (text_content[t])
+t++;
+_write = write(fot, text_content, t);
+if (_write == -1 || _write < t)
+{
+close(fot);
+return (-1);
+}
+}
+if (close(fot) == -1)
+{
+return (-1);
+}
+return (1);
 }
