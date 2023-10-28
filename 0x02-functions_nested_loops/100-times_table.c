a2ddc8ffd199c74c874853917275d18034f4a7e9
Linus Atte <linuseatte@gmail.com>
2023-10-28 08:16:16 -0700

    Update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,65 +1,47 @@
-#include "main.h"
+#include "holberton.h"
 /**
- * print_spaces - This prints commas and spaces
- * @n: is a integer define the number of spaces
-(* a blank line
-* Description: This prints spaces)?
-* Return: No return a void function.
- */
-void print_spaces(int n)
-{
-	int i;
-
-	_putchar(',');
-
-	for (i = 0; i < n; i++)
-	{
-		_putchar(' ');
-	}
-}
-/**
- * print_times_table - This prints times table
- * @n: The main table to print
- * a blank line
- * Description: this function print times table)?
- * section header: the header file for this function is main.h
- * Return: No return a void function.
+ * print_times_table - this prints the times table for n.
+ * @n: The multiplication table requested.
+ * Return: Nothing.
  */
 void print_times_table(int n)
 {
-	int table, number;
+	int i, j, res;
 
-	if (n < 0 || n > 15)
-		return;
-	for (table = 0; table <= n; table++)
+	if (!(n > 15 || n < 0))
 	{
-		for (number = 0; number <= n; number++)
+		for (i = 0; i <= n; i++)
 		{
-			int r;
-
-			r = table * number;
-			if (number && r < 10)
-				print_spaces(3);
-			else if (r >= 10 && r < 100)
-				print_spaces(2);
-			else if (r >= 100)
-				print_spaces(1);
-			if (r >= 10 && r < 100)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar((r / 10) + '0');
-				_putchar((r % 10) + '0');
+				res = (i * j);
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (res < 10 && j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((res % 10) + '0');
+				}
+				else if (res >= 10 && res < 100)
+				{
+					_putchar(' ');
+					_putchar((res / 10) + '0');
+					_putchar((res % 10) + '0');
+				}
+				else if (res >= 100 && j != 0)
+				{
+					_putchar((res / 100) + '0');
+					_putchar((res / 10) % 10 + '0');
+					_putchar((res % 10) + '0');
+				}
+				else
+					_putchar((res % 10) + '0');
 			}
-			else if (r >= 100)
-			{
-				_putchar((r / 100) + '0');
-				_putchar(((r / 10) % 10) + '0');
-				_putchar((r % 10) + '0');
-			}
-			else
-			{
-				_putchar(r + '0');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
